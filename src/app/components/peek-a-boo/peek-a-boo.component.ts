b8177292c95a44d0ec7ffd9279ac6319779a4037
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-06-26 23:47:57 -0400

    updated: peekABoo component

--- src/app/components/peek-a-boo/peek-a-boo.component.ts
+++ src/app/components/peek-a-boo/peek-a-boo.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentChecked, AfterContentInit, AfterViewInit, AfterViewChecked, OnDestroy, Input } from '@angular/core';
+import { LoggerService } from 'app/services/logger.service';
+
+let nextId =1;
 
 @Component({
   selector: 'app-peek-a-boo',
   templateUrl: './peek-a-boo.component.html',
   styleUrls: ['./peek-a-boo.component.scss']
 })
-export class PeekABooComponent implements OnInit {
 
-  constructor() { }
+// Don't HAVE to mention the Lifecycle Hook interfaces
+// unless we want typing and tool support.
+export class PeekABooComponent extends PeekABoo implements
+             OnChanges, OnInit, DoCheck,
+             AfterContentInit, AfterContentChecked,
+             AfterViewInit, AfterViewChecked,
+             OnDestroy {
+  // implement OnInit's `ngOnInit` method
+  ngOnInit() { this.logIt(`OnInit`); }
 
-  ngOnInit() {
+  logIt(msg: string) {
+    this.logger.log(`#${nextId++} ${msg}`);
   }
+  }
+  @Input() name: string;
 
-}
+  private verb = 'initialized';
+
+  constructor(logger: LoggerService) {
+    super(logger);
+
+    let is = this.name ? 'is' : 'is not';
+    this.logIt(`name ${is} known at construction`);
+  }
